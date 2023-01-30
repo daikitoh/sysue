bf4beb67428ec1a49411e47e29602b6da2ebcc7f
daikiitos <101114485+daikiitos@users.noreply.github.com>
2023-01-30 14:17:01 +0900

    bug fix

--- schemas/recipe.py
+++ schemas/recipe.py
@@ -2,30 +2,6 @@ from typing import Optional, List
 from pydantic import BaseModel
 import json
 
-# For post
-class RequestRecipeBase(BaseModel):
-    category_id: int
-    title: str
-    description: Optional[str]
-    servings: int
-    ingredients: List[RecipeIngredientBase] #本来idで指定
-    allergens: Optional[List[int]]
-    tags: Optional[List[str]]
-    instructions: List[str]
-
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate_to_json
-
-    @classmethod
-    def validate_to_json(cls, value):
-        if isinstance(value, str):
-            return cls(**json.loads(value))
-        return value
-
-    # class Config:
-    #     orm_mode = True
-
 #  For Responses
 class IngredientBase(BaseModel):
     id: int
@@ -78,3 +54,27 @@ class RecipeBase(BaseModel):
 
     class Config:
         orm_mode = True
+
+# For post
+class RequestRecipeBase(BaseModel):
+    category_id: int
+    title: str
+    description: Optional[str]
+    servings: int
+    ingredients: List[RecipeIngredientBase] #本来idで指定
+    allergens: Optional[List[int]]
+    tags: Optional[List[str]]
+    instructions: List[str]
+
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate_to_json
+
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
+
+    # class Config:
+    #     orm_mode = True
